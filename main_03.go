83cadb13e2c869b0e30c301c6c7b45e95fef3a30
unknown <64209010009@nawamin.ac.th>
2025-01-13 16:16:07 +0700

    first commit

--- main_03.go
+++ main_03.go
@@ -4,13 +4,14 @@ import (
  EmployeeController "backend/api/controller/employee"
 
  "github.com/gin-gonic/gin"
+  
 )
 
 func main() {
  router := gin.Default()
  //Employee API Method
  router.GET("/employee", EmployeeController.GetEmployee)            //GET
- router.GET("/employee/:id", EmployeeController.GetEmployeebyID)    //GET BY ID
+ router.GET("/employee/:id", EmployeeController.GetEmployeeByID)    //GET BY ID
  router.POST("/employee", EmployeeController.PostEmployee)          //POST
  router.PUT("/employee", EmployeeController.PutEmployee)            //PUT
  router.DELETE("/employee", EmployeeController.DeleteEmployee)      //DELETE
